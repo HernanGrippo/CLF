fe7e0c0e37ed28fe4196f9ce02d7ec551f4f6b15
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-04-01 01:49:28 +0200

    wordpress integration

---
+++ wp-content/themes/clfc/header.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html <?php language_attributes(); ?>> 
+<head>
+<title>Conservation Lands Foundation Campaign</title>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://use.typekit.net/env8cis.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css"/>
+
+<style>
+
+p#hero {
+  font-size: 24px;
+}
+
+.logo {
+    max-width: 281px;
+    height: auto;
+}
+
+.navbar {
+    height: 200px;
+}
+
+.navbar-nav {
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+
+.navbar-nav .nav-link {
+  font-style: normal;
+  font-weight: bold;
+  color: #9B3926;
+  font-size: 26px;
+  line-height: 31.47px;
+  text-transform: uppercase;
+  font-family: 'Inter', sans-serif;
+  margin-left: 15px;
+}
+
+.line {
+  position: relative;
+  width: 100%;
+  height: 3px;
+  border: 3px solid #C34F30;
+  transform: rotate(180deg);
+}
+
+.container {
+  max-width: 1600px;
+  margin: 0 auto;
+}
+
+i {
+  font-size: 24px;
+  color: #000;
+}
+
+.font-style-normal {
+  font-style: normal;
+}
+
+.font-weight-400 {
+  font-weight: 400;
+}
+
+.font-size-26 {
+  font-size: 26px;
+}
+
+#video iframe {
+  width: 100%;
+  min-height: 500px;
+}
+
+</style>
+<?php wp_head(); ?>
+</head>
+<body class="body">
+<!-- Navbar -->
+<header>
+<div class="container">
+    <div class="row">
+      <div class="col">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <!-- Logo -->
+            <a class="navbar-brand" href="#">
+              <img class="logo" src="<?php bloginfo('template_url'); ?>/img/clfc-logo.png" alt="Conservation Lands Foundation Campaign logo">
+            </a>
+  
+            <!-- Toggle button for collapsed menu -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+  
+            <!-- Menu items -->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Get Involved</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Resources</a>
+                  </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+</div>
+</header>
+<!-- end of Navbar -->
+
+<div class="line"></div>
+
+<div class="container background">
