c52ae08d0c4d798b14dbc9486597a1b6527e9ea4
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-04-06 02:06:55 +0200

    Footer done, links, national logo, logo size fixed, social media icons, all hovers created

--- wp-content/themes/clfc/footer.php
+++ wp-content/themes/clfc/footer.php
@@ -5,17 +5,24 @@
   <footer class="footer">
     <div class="row justify-content-center">
       <div class="col-md-6">
-        <img src="<?php bloginfo('template_url'); ?>/img/logo-footer.png" alt="Your Logo" class="me-3" style="max-width: 281px;"> 
+        <img src="<?php bloginfo('template_url'); ?>/img/logo-footer.png" alt="conservation lands foundation logo" class="me-3" style="max-width: 461px;"> 
+        <img src="<?php bloginfo('template_url'); ?>/img/national.png" alt="national system of public lands" class="me-3" style="max-width: 234px;"> 
         <div class="d-flex align-items-center">
           <div class="contact-links">
             <p><a href="mailto:info@conservationlands.org">info@conservationlands.org</a></p>
-            <p><a href="tel:+19702470807">(970) 247-0807</a></p>
-            <a href="https://www.facebook.com/" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/facebooklogo.png" alt="Your Logo" class="me-3" style="max-width: 37px;"></a> 
-            <a href="https://www.instagram.com/" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/instagramlogo.png" alt="Your Logo" class="me-3" style="max-width: 37px;"></a> 
+            <a href="https://www.facebook.com/conservationlands/" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" alt="facebook" class="me-3" style="max-width: 20px;"></a> 
+            <a href="https://twitter.com/ConservationLF" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" alt="twitter" class="me-3" style="max-width: 34px;"></a> 
+            <a href="https://www.instagram.com/conservationlands/" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/instagram.png" alt="instagram" class="me-3" style="max-width: 30px;"></a> 
+            <a href=https://www.youtube.com/user/ConservationLF" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/youtube.png" alt="youtube" class="me-3" style="max-width: 44px;"></a> 
+            <a href="https://www.linkedin.com/company/conservation-lands-foundation/" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/linkedin.png" alt="linkedin" class="me-3" style="max-width: 30px;"></a> 
+            <a href="https://www.flickr.com/photos/conservationlands" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/vector.png" alt="flickr" class="me-3" style="max-width: 29px;"></a> 
+            <a href="https://www.tiktok.com/@conservationlands" target="_blank"> <img src="<?php bloginfo('template_url'); ?>/img/tiktok.png" alt="tiktok" class="me-3" style="max-width: 30px;"></a> 
           </div>
         </div>
-        <p>We acknowledge that the land on which our home office is located and where members of our staff live and execute our business is the original land of the Utes–Nuuchiu–the longest continuous residents of Colorado.</p>
-        <p>© 2023. All Rights Reserved.	Privacy Policy. Terms and Conditions.</p>
+        <div class="col-md-9">
+          <p>We acknowledge that the land on which our home office is located and where members of our staff live and execute our business is the original land of the Utes–Nuuchiu–the longest continuous residents of Colorado.</p>
+          <p>© 2023. All Rights Reserved.	Privacy Policy. Terms and Conditions.</p>
+        </div>
       </div>
       <div class="col-md-2 offset-md-4">
         <ul>
