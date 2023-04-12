183a29bfa51cc44fbc6837c8be751bf49cc3e331
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-04-12 03:37:49 +0200

    About us hero and css fixes

--- wp-content/themes/clfc/about.php
+++ wp-content/themes/clfc/about.php
@@ -6,19 +6,8 @@ Template Name: About
 <?php get_header();?>
 
 <!-- HERO -->
-<section id="hero">
-    <div class="row align-items-center">
-      <div class="col-md-2">
-        <div class="text-center d-sm-none d-md-block">
-          <img src="<?php bloginfo('template_url'); ?>/img/waves.png" alt="waves">
-        </div>
-      </div>
-      <div class="col-md-8 text-center">
-        <h1 class="text-center">About</h1>
-      </div>
-      <div class="col-md-2 text-center">
-        <div class="text-center d-sm-none d-md-block">
-          <img src="<?php bloginfo('template_url'); ?>/img/waves.png" alt="waves">
-        </div>
-      </div>
+<section id="hero-aboutus">
+    <div class="container">
+        <h1>About</h1>
     </div>
+</section>
