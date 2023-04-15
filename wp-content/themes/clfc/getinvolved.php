d3a54ef43f0470273b99f052d87dad60fc84c6b1
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-04-15 03:30:10 +0200

    get involved page draft

---
+++ wp-content/themes/clfc/getinvolved.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Template Name: GetInvolved
+*/
+?>
+<?php get_header();?>
+
+<!-- HERO -->
+<section id="hero-getinvolved">
+    <div class="container">
+        <h1>Respect.Connect.Protect.</h1>
+        <p>Led by Conservation Lands Foundation, in partnership with the Bureau of Land Management and an array of local and indigenous affiliate groups, “Respect. Connect. Protect.” is a campaign designed to encourage respectful and safe visitation on America’s National Conservation Lands.</p>
+        <p>The coalition works collaboratively with partners to provide resources for understanding the uses, history, and Indigenous cultural significance of the land as well as specific regional guidance for how to make visits safe and enjoyable. </p>    
+    </div>
+</section>
+
+<!-- WHAT ARE NATIONAL SECTION -->
+<section id="meet">
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <h2>Meet Spokespebble</h2>
+      <p>As strong as Smokey the Bear, as wise as Woodsy the Owl, Spokespebble is a nature-loving hunk of million-year-old sedimentary rock who is young at heart and always on the go. As the official voice (and adorable face) of the Respect. Connect. Protect. campaign, Spokespebble is your guide to planning ahead, staying safe, and being awesome whenever you visit National Conservation Lands.</p>
+      <p>Managed by the Bureau of Land Management, many of these protected lands are beyond and between the National Parks and include:</p>
+        <ul>
+         <li>Favorite food: Rock candy.</li>
+         <li>Favorite movie: The Rock.</li>
+         <li>Wilderness Areas</li>
+         <li>Favorite genre of comedy: Rock puns.</li>
+         <li>Favorite music: Smooth jazz. (Psych. It’s rock music.)</li>
+        </ul> 
+    </div>
+    <div class="col-md-4">
+      <img src="<?php bloginfo('template_url'); ?>/img/rockyinvolved.png" alt="cactus and desert image" class="img-fluid">
+    </div>
+  </div>
+</div>
+</section>
+
+<!-- Video -->
+  <section id="video">
+    <div class="row">
+      <div class="col text-center">
+        <img src="<?php bloginfo('template_url'); ?>/img/video.png" alt="Video" class="img-fluid rounded">
+      </div>
+    </div>
+  </section>
+
+  <!-- HERO -->
+   <section id="hero">
+    <div class="row align-items-center">
+      <div class="col-md-2">
+        <div class="text-center d-sm-none d-md-block">
+        </div>
+      </div>
+      <div class="col-md-8 text-center">
+      <img class="waveabout" src="<?php bloginfo('template_url'); ?>/img/waveabout.png" alt="wave line">
+        <h2 class="text-center">Join in on the fun with Spokespebble</h2>
+      </div>
+      <div class="col-md-2 text-center">
+        <div class="text-center d-sm-none d-md-block">
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <p>Click the link below to bring a little Spokespebble into your life. Conservation Lands Foundation has Spokespebble GIFs, memes, postcards and other fun social media graphics for you to share on your channels. #RespectConnectProtect</p>
+        <button class="btn">DOWNLOAD</button>
+      </div>
+    </div>
+
+  </section>
+
+
+
+<?php get_footer();?> 
