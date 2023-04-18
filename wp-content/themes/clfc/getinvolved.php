9539ebbe43aef47983aa8ae19ee09d50e661460f
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-04-18 03:00:43 +0200

    Get involved Tablet view done

--- wp-content/themes/clfc/getinvolved.php
+++ wp-content/themes/clfc/getinvolved.php
@@ -28,7 +28,7 @@ Template Name: GetInvolved
 <section id="meet">
 <div class="container">
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-12 col-lg-8">
       <h2>Meet Spokespebble</h2>
       <p>As strong as Smokey the Bear, as wise as Woodsy the Owl, Spokespebble is a nature-loving hunk of million-year-old sedimentary rock who is young at heart and always on the go. As the official voice (and adorable face) of the Respect. Connect. Protect. campaign, Spokespebble is your guide to planning ahead, staying safe, and being awesome whenever you visit National Conservation Lands.</p>
       <p>Managed by the Bureau of Land Management, many of these protected lands are beyond and between the National Parks and include:</p>
@@ -40,8 +40,8 @@ Template Name: GetInvolved
          <li>Favorite music: Smooth jazz. (Psych. It’s rock music.)</li>
         </ul> 
     </div>
-    <div class="col-md-4">
-      <img src="<?php bloginfo('template_url'); ?>/img/rockyinvolved.png" alt="cactus and desert image" class="img-fluid">
+    <div class="col-md-12 col-lg-4">
+      <img src="<?php bloginfo('template_url'); ?>/img/rockyinvolved.png" alt="stone" class="img-fluid">
     </div>
   </div>
 </div>
@@ -59,22 +59,22 @@ Template Name: GetInvolved
   <!-- JOIN Spokespebble -->
    <section id="joinspoke">
     <div class="row align-items-center">
-      <div class="col-md-2">
+      <div class="col-md-0 col-lg-2">
         <div class="text-center d-sm-none d-md-block">
         </div>
       </div>
-      <div class="col-md-8 text-center">
+      <div class="col-md-12 col-lg-8 text-center">
       <img class="waveaboutspoke" src="<?php bloginfo('template_url'); ?>/img/waveabout.png" alt="wave line">
         <h2 class="text-center">Join in on the fun with Spokespebble</h2>
       </div>
-      <div class="col-md-2 text-center">
+      <div class="col-md-0 col-lg-2 text-center">
         <div class="text-center d-sm-none d-md-block">
         </div>
       </div>
     </div>
 
     <div class="row">
-      <div class="col-md-8 offset-md-2">
+      <div class="col-md-12 col-lg-8 offset-lg-2">
         <p>Click the link below to bring a little Spokespebble into your life. Conservation Lands Foundation has Spokespebble GIFs, memes, postcards and other fun social media graphics for you to share on your channels. #RespectConnectProtect</p>
         <button class="btn">DOWNLOAD</button>
       </div>
