a7301425a747af7029ba022e314ea6aa7134fac9
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-04-19 00:41:23 +0200

    QA CLFC-28

--- wp-content/themes/clfc/index.php
+++ wp-content/themes/clfc/index.php
@@ -2,30 +2,18 @@
 
  <!-- HERO -->
 <section id="hero">
-    <div class="row align-items-center">
-      <div class="col-md-2">
-        <div class="text-center d-sm-none d-md-block">
-          <img src="<?php bloginfo('template_url'); ?>/img/waves.png" alt="waves">
-        </div>
-      </div>
-      <div class="col-md-8 text-center">
+  <div class="row align-items-center">
+      <div class="col-md-12 text-center">
         <h1 class="text-center">Respect. Connect. Protect</h1>
       </div>
-      <div class="col-md-2 text-center">
-        <div class="text-center d-sm-none d-md-block">
-          <img src="<?php bloginfo('template_url'); ?>/img/waves.png" alt="waves">
-        </div>
-      </div>
-</div>
-
+  </div>
     <div class="row">
       <div class="col-md-8 offset-md-2">
         <p>Led by Conservation Lands Foundation, the Bureau of Land Management, and an array of local and indigenous affiliate groups, “Respect. Connect. Protect.” is a campaign designed. to encourage enthusiastic, respectful, and safe usage of National Conservation Lands.</p>
         <button class="btn">LEARN MORE</button>
       </div>
     </div>
-
-  </section> 
+</section> 
 
   <!-- Video -->
   <section id="video">
