ee5b578bf9ed63b9257d8460bf36c7ef819812f7
Josue Mora <josue@gocodistry.com>
2023-05-01 11:00:23 -0700

    Styles correction, added follow us text

--- wp-content/themes/clfc/about.php
+++ wp-content/themes/clfc/about.php
@@ -48,45 +48,53 @@ Template Name: About
 
 <!--LOGOS AND ABOUT -->
 <section id="logoabout">
-<div class="container">
-  <div class="row">
-    <div class="col-md-12 col-lg-3">    
-      <?php 
-            $image = get_field('conservation_icon');
-            if( !empty( $image ) ): ?>
-            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="clfc-logo img-fluid"/>
-      <?php endif; ?>
-    </div>
-    <div class="col-md-12 col-lg-8">
-      <h3><?php the_field('about_conservation_title'); ?></h3>
-      <p><?php the_field('about_conservation_paragraph'); ?></p>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col-md-12 col-lg-3">
-      <?php 
-            $image = get_field('about_bureau_icon');
-            if( !empty( $image ) ): ?>
-            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="blm-logo img-fluid"/>
-      <?php endif; ?>
-    </div>
-    <div class="col-md-12 col-lg-9">
-      <h3><?php the_field('about_bureau_title'); ?></h3>
-      <p><?php the_field('about_bureau_paragraph'); ?></p>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-lg-3">    
+        <?php 
+              $image = get_field('conservation_icon');
+              if( !empty( $image ) ): ?>
+              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="clfc-logo img-fluid"/>
+        <?php endif; ?>
+      </div>
+      <div class="col-md-12 col-lg-8">
+        <h3><?php the_field('about_conservation_title'); ?></h3>
+        <p><?php the_field('about_conservation_paragraph'); ?></p>
+      </div>
     </div>
   </div>
-</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-lg-3">
+        <?php 
+              $image = get_field('about_bureau_icon');
+              if( !empty( $image ) ): ?>
+              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="blm-logo img-fluid"/>
+        <?php endif; ?>
+      </div>
+      <div class="col-md-12 col-lg-9">
+        <h3><?php the_field('about_bureau_title'); ?></h3>
+        <p><?php the_field('about_bureau_paragraph'); ?></p>
+      </div>
+    </div>
+  </div>
 </section>
 
 <!--LOGOS AND ABOUT -->
 <section id="instagram-embed">
+  <div class="container">
     <div class="row align-items-center">
         <div class="col-md-10 offset-md-1">
             <iframe src="https://www.instagram.com/conservationlands/embed" width="100%" height="530" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
         </div>
     </div>
+    <div class="row">
+      <div class="col">
+        <div class="vertical-spacer-sm ">&nbsp;</div>
+        <p class="text-center">Follow us <span>@conservationlands</span></p>
+      </div>
+    </div>
+  </div>
 </section>
 
 <?php get_footer();?>
