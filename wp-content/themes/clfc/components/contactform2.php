f30bbae822377bd72fc9e3450323f1c8f539ac2c
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-05-03 21:01:35 +0200

    [CLFC-74] Contact page updated, Form, Title, Paragraph desktop and responsive

---
+++ wp-content/themes/clfc/components/contactform2.php
@@ -0,0 +1,18 @@
+  <!-- CONTACT FORM -->
+  <section id="contact-form">
+    <div class="row">
+      <div class="col">
+        <div class="container">
+          <div class="left">
+            <h2>Sign up for your Spokespebble updates!</h2>
+          </div>
+            <div class="right text-center">
+                <div class="form">
+                   <?php echo apply_shortcodes('[contact-form-7 id="11" title="Contact form 1"]'); ?>
+                </div>
+                  <img src="https://www.respectconnectprotect.org/wp-content/uploads/2023/04/stone.png" alt="happy stone">
+            </div>
+          </div> 
+        </div>
+    </div>
+  </section>
