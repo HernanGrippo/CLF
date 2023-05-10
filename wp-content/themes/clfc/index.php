ff4dd0a4ed69ec4ca084f3af5736a5a6d29c1496
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-05-10 20:35:55 +0200

    [CLFC-60]Push name test

--- wp-content/themes/clfc/index.php
+++ wp-content/themes/clfc/index.php
@@ -125,7 +125,7 @@ Template Name: Home
   <!-- CONTACT FORM -->
   <? get_template_part( 'components/contactform' ); ?>
   
- <!-- PARTNER SECTION -->
+ <!-- PARTNERS SECTION -->
     <section id="partners">
     <div class="row">
       <div class="col-12 text-center">
