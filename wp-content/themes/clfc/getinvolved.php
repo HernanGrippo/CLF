fb4a99398fb401a941c7c360c0f44869397de6ea
Hernan Grippo <hernan.grippo@gocodistry.com>
2023-05-17 04:18:40 +0200

    [CLFC-7]-MultiPlayer-NewFeature-WIP

--- wp-content/themes/clfc/getinvolved.php
+++ wp-content/themes/clfc/getinvolved.php
@@ -40,48 +40,59 @@ Template Name: GetInvolved
 
 <div class="vertical-spacer-sm">&nbsp;</div>
 
-<!-- Video Embed --> 
-<!-- Image -->
-<section id="video_embed">
+<!-- SUPER PLAYER --> 
+<section id="superplayer">
+  <div class="container">
     <div class="row">
-      <div class="col-12 text-center">
-      <?php 
-          $image = get_field('get_involved_video_image');
-          if( !empty( $image ) ): ?>
-              <img tabindex="12" class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" id="thumbnail"/>
-      <?php endif; ?>
-<!-- Video -->
-        <div id="videoContainer" style="display: none;">
-            <?php
-
-            // Load value.
-            $iframe = get_field('video_embed_url');
-
-            // Use preg_match to find iframe src.
-            preg_match('/src="(.+?)"/', $iframe, $matches);
-            $src = $matches[1];
-
-            // Add extra parameters to src and replace HTML.
-            $params = array(
-                'controls'  => 1,
-                'hd'        => 1,
-                'autohide'  => 1,
-                'autoplay'=> 0,
-            );
-            $new_src = add_query_arg($params, $src);
-            $iframe = str_replace($src, $new_src, $iframe);
-
-            // Add extra attributes to iframe HTML.
-            $attributes = 'frameborder="0"';
-            $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
-
-            // Display customized HTML.
-            echo $iframe;
-            ?>
+      <div class="col-lg-4">
+        <div class="spacesforline">
+        <div class="list-group">
+          <h3>MORE VIDEOS</h3>
+          <button class="list-group-item list-group-item-action d-flex align-items-center" onclick="playVideo('LvYehnKonDA')">
+            <img src="<?php bloginfo('template_url'); ?>/img/videolist1.png" alt="Image 1" class="img-fluid mr-3">
+            <div class="flex-grow-1">
+              <p class="mb-0">It's On You | Respect. Connect. Protect.</p>
+            </div>
+          </button>
+          <button class="list-group-item list-group-item-action d-flex align-items-center" onclick="playVideo('D02Hp2c2qTU')">
+            <img src="<?php bloginfo('template_url'); ?>/img/videolist2.png" alt="Image 2" class="img-fluid mr-3">
+            <div class="flex-grow-1">
+              <p class="mb-0">Protect What Matters: Conservation Lands Foundation</p>
+            </div>
+          </button>
+          <button class="list-group-item list-group-item-action d-flex align-items-center" onclick="playVideo('eOBDOZH3nns')">
+            <img src="<?php bloginfo('template_url'); ?>/img/videolist3.png" alt="Image 3" class="img-fluid mr-3">
+            <div class="flex-grow-1">
+              <p class="mb-0">Respect. Connect. Protect. | Conserve Public Lands</p>
+            </div>
+          </button>
+          <button class="list-group-item list-group-item-action d-flex align-items-center" onclick="playVideo('jqS44UH7Nrs')">
+            <img src="<?php bloginfo('template_url'); ?>/img/videolist4.png" alt="Image 4" class="img-fluid mr-3">
+            <div class="flex-grow-1">
+              <p class="mb-0">Respect. Connect. Protect. A Word from Spokespebble</p>
+            </div>
+          </button>
+          <button class="list-group-item list-group-item-action d-flex align-items-center" onclick="playVideo('-29icN6oAJ0')">
+            <img src="<?php bloginfo('template_url'); ?>/img/videolist5.png" alt="Image 5" class="img-fluid mr-3">
+            <div class="flex-grow-1">
+              <p class="mb-0">President Biden Honors Avi Kwa Ame</p>
+            </div>
+          </button>
+        </div>
+      </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="video-player d-flex align-items-center justify-content-center">
+        <div id="imageOverlay">
+            <img src="<?php bloginfo('template_url'); ?>/img/supervideoplayer.png" alt="Static Image" class="overlay-image">
+          </div>
+          <div id="youtubePlayer"></div>
+        </div>
       </div>
     </div>
   </div>
 </section>
+
 
   <!-- JOIN Spokespebble -->
   <section id="joinspoke">
